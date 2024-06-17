14078f3bcb2c083fd93fa984c737ad8f36a926ec
Daniel-Obuya <daniel.nyabuto@strathmore.edu>
2024-06-17 23:15:48 +0300

    first commit

---
+++ src/ATM.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class ATM {
+    public static void main(String[] args)throws IOException {
+        OptionMenu optionMenu = new OptionMenu();
+        optionMenu.getLogin();
+    }
+    
+}
